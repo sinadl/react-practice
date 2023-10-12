400c561256d2d8f1a64c2ea9fe63d1c032cc9538
sina <sinadsina78@gmail.com>
2023-10-12 16:56:48 +0330

    change --ver 1.1.1
    -- build structure of project

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,8 +1,23 @@
-
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+import Homepage from './pages/Homepage'
+import Pricing from './pages/Pricing'
+import Product from './pages/Product'
+import AppLayout from './pages/AppLayout'
+import PageNotFound from './pages/PageNotFound'
+import Login from './pages/Login'
 
 export default function App() {
 
   return (
-    <div>WorldWise</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />}/>
+        <Route path="pricing" element={<Pricing />}/>
+        <Route path="product" element={<Product />}/>
+        <Route path="app" element={<AppLayout />}/>
+        <Route path="login" element={<Login />}/>
+        <Route path="*" element={<PageNotFound />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ worldwise/src/pages/AppLayout.jsx
@@ -0,0 +1,7 @@
+// import styles from './AppLayout.module.css'
+
+export default function AppLayout() {
+  return (
+    <div>AppLayout</div>
+  )
+}
